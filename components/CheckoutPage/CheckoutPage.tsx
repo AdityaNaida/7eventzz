5d9284989df001642beba965bf28f76137513b70
AdityaNaida <adityanaida@gmail.com>
2024-05-02 18:51:37 +0530

    [ERROR] solving

--- components/CheckoutPage/CheckoutPage.tsx
+++ components/CheckoutPage/CheckoutPage.tsx
@@ -1,6 +1,3 @@
-"use client";
-import { useRef } from "react";
-
 import Steps from "../CartPage/Steps";
 import CheckoutData from "./CheckoutData";
 import CheckoutBill from "./CheckoutBill";
@@ -9,14 +6,6 @@ import CheckoutBillStatement from "./CheckoutBillStatement";
 import style from "@/components/CheckoutPage/CheckoutPage.module.css";
 
 export default function CheckoutPage() {
-  const billStatement = useRef<HTMLDivElement>(null);
-
-  function billFollower() {
-    window.scrollTo({
-      top: billStatement.current?.offsetHeight || 0,
-      behavior: "smooth",
-    });
-  }
   return (
     <>
       <div className={style.container}>
@@ -25,8 +14,8 @@ export default function CheckoutPage() {
           <CheckoutData />
         </div>
         <div className={style.bill}>
-          <CheckoutBill followerBtn={billFollower} />
-          <CheckoutBillStatement billRef={billStatement} />
+          <CheckoutBill />
+          <CheckoutBillStatement />
         </div>
       </div>
     </>
